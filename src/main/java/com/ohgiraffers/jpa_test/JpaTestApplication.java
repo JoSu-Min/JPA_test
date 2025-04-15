d55526af6ede20a75adece9b9ed562de7ff0d2dc
조수민 <skg04064@gmaile.com>
2025-04-15 23:34:43 +0900

    finish

--- src/main/java/com/ohgiraffers/jpa_test/JpaTestApplication.java
+++ src/main/java/com/ohgiraffers/jpa_test/JpaTestApplication.java
@@ -1,7 +1,12 @@
 package com.ohgiraffers.jpa_test;
 
+import com.ohgiraffers.jpa_test.domain.MemberEntity;
+import com.ohgiraffers.jpa_test.dto.MemberDTO;
+import com.ohgiraffers.jpa_test.rep.MemberRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaTestApplication {
@@ -10,4 +15,11 @@ public class JpaTestApplication {
         SpringApplication.run(JpaTestApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner run(MemberRepo repo) {
+        return args -> {
+            MemberDTO dto = new MemberDTO(null, "testuser", "홍길동", "1234", 30);
+            repo.save(new MemberEntity(dto));
+        };
+    }
 }
